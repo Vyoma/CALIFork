b89cb2995a8f582c5526a1a03bac1581a9a8dd0c
Austin Riedel <riedel@Austins-MacBook-Pro-3.local>
2017-08-30 21:09:39 -0700

    Modify styling for Nav and Tab components

--- source/app/components/Tab/Tab.js
+++ source/app/components/Tab/Tab.js
@@ -6,13 +6,13 @@ import styles from '../../styles/styles.scss'
 
 export default ({ activeTabIndex, tabIndex, tabValue, handleSelectTab }) => {
 
-	const tabClassName = tabIndex === activeTabIndex ? styles[`tab active`] : styles[`tab default`]; 
-
+	const tabClassName = tabIndex === activeTabIndex ? `tab-active` : `tab`; 
+	console.log(tabClassName)
 	const _handleSelectTab = () => {
 		handleSelectTab(tabIndex, tabValue)
 	}
 
 	return (
-		<button className={tabClassName} onClick={_handleSelectTab}>{tabValue}</button>
+		<button className={styles[tabClassName]} onClick={_handleSelectTab}>{tabValue}</button>
 	)
 }
--- source/app/containers/Nav/index.js
+++ source/app/containers/Nav/index.js
@@ -8,19 +8,22 @@ import { Link } from 'react-router-dom';
 import { Container, Row } from 'react-grid-system'
 import Col from '../../components/Column' // CUSTOM COLUMN -> OPTIMIZED FOR SERVERS SIDE RENDERING
 
+// STYLES
+import styles from '../../styles/styles.scss'
+
 export default () => (
-	<Container fluid={true} className="tl-nav-container">
+	<Container fluid={true} className={styles["tl-nav-container"]}>
 		<Row style={{ height: '100%', justifyContent: 'center', margin: 0}}>
-			<Col sm={7} md={7} className="tl-nav-brand">
-				<h2 className="tl-nav-header">
+			<Col sm={7} md={7} className={styles["tl-nav-brand"]}>
+				<h2 className={styles["tl-nav-header"]}>
 					Cognitive Asset Library
 				</h2>
 			</Col>
-			<Col sm={5} md={5} className="tl-nav-right">
-			  <Link to={`/`} className="tl-nav-button" id="header-link2" kind='secondary'>Home</Link>
-			  <Link to={`/`} className="tl-nav-button" id="header-link1" kind='secondary'>Engage Us</Link>
-			  <Link to={`/`} className="tl-nav-button" id="header-link1" kind='secondary'>About Us</Link>
-			  <Link to={`/publish`} className="tl-nav-button" id="header-link1" kind='secondary'>Publish</Link>
+			<Col sm={5} md={5} className={styles["tl-nav-right"]}>
+			  <Link to={`/`} className={styles["tl-nav-button"]} id="header-link2" kind='secondary'>Home</Link>
+			  <Link to={`/`} className={styles["tl-nav-button"]} id="header-link1" kind='secondary'>Engage Us</Link>
+			  <Link to={`/`} className={styles["tl-nav-button"]} id="header-link1" kind='secondary'>About Us</Link>
+			  <Link to={`/publish`} className={styles["tl-nav-button"]} id="header-link1" kind='secondary'>Publish</Link>
 			</Col>
 		</Row>
 	</Container>
