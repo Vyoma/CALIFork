e6ef17538b3e924cb8f238f96d1c65baf27b187d
Austin Riedel <riedel@Austins-MacBook-Pro-3.local>
2017-09-01 10:34:56 -0700

    Implement route for PublishedAssetPage

--- source/client/routes.js
+++ source/client/routes.js
@@ -2,6 +2,7 @@ import App from '../app/components/App'
 import Home from '../app/containers/Home/HomePage'
 import Asset from '../app/containers/Asset/AssetPage'
 import Publish from '../app/containers/Publish/PublishPage'
+import PublishedAsset from '../app/containers/PublishedAsset'
 import About from '../app/containers/About/AboutPage'
 
 const routes = [
@@ -16,6 +17,9 @@ const routes = [
       },
       { path: '/publish',
         component: Publish
+      },
+      { path: '/publishedasset',
+        component: PublishedAsset
       },
       { path: '/about',
         component: About
