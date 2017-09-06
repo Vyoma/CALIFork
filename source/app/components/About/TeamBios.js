7045b8570c9b08f36d7c801b5dafd801af884c18
Trinh N. Le <Trinh.Le@ibm.com>
2017-09-06 09:51:34 -0500

    team overview done

--- source/app/components/About/TeamBios.js
+++ source/app/components/About/TeamBios.js
@@ -19,6 +19,56 @@ const TeamBios = () => {
   };
   return (
     <div>
+      {/* <Col md={12}>   
+        <Row>
+          <div className={styles['tl-team-bio']}>
+            <label className='bx--label'>
+              <h2 className={styles['tl-team-bio-title']}>Meet the Team</h2>
+            </label>
+            <div className={styles['tl-team-bio-line']}></div>
+              <p className={styles['example-p-style']}> 
+                Pumpkin spice Write Me A Song Country. Music Hall of Fame write a song about pumpkin spice. Cats vote for Taylor trouble Loft 89. Available at Target best dress best dress Meredith. Grey enchanted I'm on the bleachers sparkly dress girlnextdooritis Karlie. Kloss Kanye West Famous Loft 89 Emma Stone available at. Target Lily Aldridge Tribeca gym Ed Sheeran Jake Gyllenhaal Tree. Paine situation Christmas music darling I'm a nightmare. Dressed like a daydream 22 for me I think Met Gala. Romeo so basically Katy Perry mean refrigerator light. 
+              </p>
+          </div>
+        </Row>
+            <Row>
+              <div className={styles['tl-team-overview']}>
+              <Accordion>
+                <AccordionItem 
+                  title="Our Mission Statement" 
+                  open ={false}> 
+                  <p>To drive Product and Engineering Eminence in our service offerings in Cognitive.</p>
+                </AccordionItem>
+
+                <AccordionItem 
+                  title="Who are We?" 
+                  open ={false}> 
+                  <p>.</p>
+                </AccordionItem>
+
+                <AccordionItem 
+                  title="What are our Skills?" 
+                  open ={false}> 
+                  <p>We are a team of engineers, hackers, and futurists who are passionate problem solvers using technology and science. We believe in being a multi disciplinary team collectively bringing the best. We are driven by using the latest and greatest Artificial Intelligence capabilities to resolve challenging business pain points. Our unique, multi-disciplinary expertise enables us to deliver excellent technological solutions for our clients. We are actively reshaping the perception of IBM GBS in the marketplace with strong internal and external eminence from our practice-wide hackathons and innovative white papers. Our team comprises of skills needed to build end-to-end cognitive solutions starting from Storage, Compute, DevOps, ETL, Data Foundation, Analytical Modeling, Data Science, Machine Learning, Deep Learning, NLP, Enterprise Solution, Mobile Development, Cloud, SAS, Micro services, On-Premise, Process Transformation, Cognitive Assistant, Unsupervised AI enhancements, Mean Stack, NOSQL Databases, and FULL Stack application development.</p>
+                </AccordionItem>
+
+                <AccordionItem 
+                  title="What do we Do?" 
+                  open ={false}> 
+                  <p>
+                    <ul>
+                      <li>Approach and understand the problem at the source and data level</li>
+                      <li>Create a solution stack which is agnostic and leverages the architectural design principles</li>
+                      <li>Embrace a fully agile development cycle of realization</li>
+                      <li>Operate under a co-located development model</li>
+                    </ul>
+                  </p>
+                </AccordionItem>
+              </Accordion>
+              </div>
+            </Row>
+        </Col> */}
+      
       <Accordion>
         <AccordionItem 
           title="Priya Vijayarajendran | CBDS CTO" 
@@ -63,6 +113,10 @@ const TeamBios = () => {
             <br/>
         </AccordionItem>
       </Accordion>
+
+
+
+
    </div>
     )
 }
--- source/app/components/About/TeamOverview.js
+++ source/app/components/About/TeamOverview.js
@@ -27,7 +27,7 @@ const TeamOverview = () => {
           </label>
           <div className={styles['tl-team-bio-line']}></div>
             <p className={styles['example-p-style']}> 
-              Pumpkin spice Write Me A Song Country. Music Hall of Fame write a song about pumpkin spice. Cats vote for Taylor trouble Loft 89. Available at Target best dress best dress Meredith. Grey enchanted I'm on the bleachers sparkly dress girlnextdooritis Karlie. Kloss Kanye West Famous Loft 89 Emma Stone available at. Target Lily Aldridge Tribeca gym Ed Sheeran Jake Gyllenhaal Tree. Paine situation Christmas music darling I'm a nightmare. Dressed like a daydream 22 for me I think Met Gala. Romeo so basically Katy Perry mean refrigerator light. 
+            We are a team of engineers, hackers, and futurists who are passionate problem solvers using technology and science. We believe in being a multi disciplinary team collectively bringing the best. We are driven by using the latest and greatest Artificial Intelligence capabilities to resolve challenging business pain points. Our unique, multi-disciplinary expertise enables us to deliver excellent technological solutions for our clients. We are actively reshaping the perception of IBM GBS in the marketplace with strong internal and external eminence from our practice-wide hackathons and innovative white papers.
             </p>
         </div>
       </Row>
@@ -41,12 +41,6 @@ const TeamOverview = () => {
           </AccordionItem>
 
           <AccordionItem 
-            title="Who are We?" 
-            open ={false}> 
-            <p>We are a team of engineers, hackers, and futurists who are passionate problem solvers using technology and science. We believe in being a multi disciplinary team collectively bringing the best. We are driven by using the latest and greatest Artificial Intelligence capabilities to resolve challenging business pain points. Our unique, multi-disciplinary expertise enables us to deliver excellent technological solutions for our clients. We are actively reshaping the perception of IBM GBS in the marketplace with strong internal and external eminence from our practice-wide hackathons and innovative white papers.</p>
-          </AccordionItem>
-
-          <AccordionItem 
             title="What are our Skills?" 
             open ={false}> 
             <p>Our team comprises of skills needed to build end-to-end cognitive solutions starting from Storage, Compute, DevOps, ETL, Data Foundation, Analytical Modeling, Data Science, Machine Learning, Deep Learning, NLP, Enterprise Solution, Mobile Development, Cloud, SAS, Micro services, On-Premise, Process Transformation, Cognitive Assistant, Unsupervised AI enhancements, Mean Stack, NOSQL Databases, and FULL Stack application development.</p>
@@ -56,13 +50,12 @@ const TeamOverview = () => {
             title="What do we Do?" 
             open ={false}> 
             <p>
-              <ul>
-                <li>Approach and understand the problem at the source and data level</li>
-                <li>Create a solution stack which is agnostic and leverages the architectural design principles</li>
-                <li>Embrace a fully agile development cycle of realization</li>
-                <li>Operate under a co-located development model</li>
-              </ul>
+            Approach and understand the problem at the source and data level<br/>
+            Create a solution stack which is agnostic and leverages the architectural design principles<br/>
+            Embrace a fully agile development cycle of realization<br/>
+            Operate under a co-located development model<br/>
             </p>
+
           </AccordionItem>
         </Accordion>
         </div>
