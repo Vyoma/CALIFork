3fefb4d2e100cd10f44e03f14829806f77274848
Trinh N. Le <Trinh.Le@ibm.com>
2017-09-06 09:02:46 -0500

    team overview

--- source/app/components/About/TeamOverview.js
+++ source/app/components/About/TeamOverview.js
@@ -13,17 +13,22 @@ import styles from '../../styles/about.scss'
 
 const TeamOverview = () => {
   return (
-    <Col md={6}>
-      <Row>
-        <img src={placeholderImage}/>
-      </Row>
-      <Row>
-        <label className='bx--label'>CBDS AI Dev Team</label>
+    <Col md={12}>
+    <Row>
+      <img src={placeholderImage}/>
+    </Row>
+    <Row>
+      <div className={styles['tl-team-bio']}>
+        <label className='bx--label'>
+          <h2 id='tl-team-bio-title'>CBDS AI Dev Team</h2>
+        </label>
+        <div className={style['tl-team-bio-line']}></div>
         <p className={styles['example-p-style']}>
           Pumpkin spice Write Me A Song Country. Music Hall of Fame write a song about pumpkin spice. Cats vote for Taylor trouble Loft 89. Available at Target best dress best dress Meredith. Grey enchanted I'm on the bleachers sparkly dress girlnextdooritis Karlie. Kloss Kanye West Famous Loft 89 Emma Stone available at. Target Lily Aldridge Tribeca gym Ed Sheeran Jake Gyllenhaal Tree. Paine situation Christmas music darling I'm a nightmare. Dressed like a daydream 22 for me I think Met Gala. Romeo so basically Katy Perry mean refrigerator light. 
         </p>
-      </Row>
-    </Col>
+      </div>
+    </Row>
+  </Col>
   )
 }
 
