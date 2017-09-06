697d6430734e028a5fda64e57312c1d8076fd87c
Trinh N. Le <Trinh.Le@ibm.com>
2017-09-06 09:07:12 -0500

    syntax for team overview

--- source/app/components/About/TeamOverview.js
+++ source/app/components/About/TeamOverview.js
@@ -18,14 +18,14 @@ const TeamOverview = () => {
       <img src={placeholderImage}/>
     </Row>
     <Row>
-      <div className={style['tl-team-bio']}>
+      <div className={styles['tl-team-bio']}>
         <label className='bx--label'>
-          <h2 className={style['tl-team-bio-title']}>CBDS AI Dev Team</h2>
+          <h2 className={styles['tl-team-bio-title']}>CBDS AI Dev Team</h2>
         </label>
-        <div className={style['tl-team-bio-line']}></div>
-        <p className={style['example-p-style']}>
-          Pumpkin spice Write Me A Song Country. Music Hall of Fame write a song about pumpkin spice. Cats vote for Taylor trouble Loft 89. Available at Target best dress best dress Meredith. Grey enchanted I'm on the bleachers sparkly dress girlnextdooritis Karlie. Kloss Kanye West Famous Loft 89 Emma Stone available at. Target Lily Aldridge Tribeca gym Ed Sheeran Jake Gyllenhaal Tree. Paine situation Christmas music darling I'm a nightmare. Dressed like a daydream 22 for me I think Met Gala. Romeo so basically Katy Perry mean refrigerator light. 
-        </p>
+        <div className={styles['tl-team-bio-line']}></div>
+          <p className={styles['example-p-style']}> 
+            Pumpkin spice Write Me A Song Country. Music Hall of Fame write a song about pumpkin spice. Cats vote for Taylor trouble Loft 89. Available at Target best dress best dress Meredith. Grey enchanted I'm on the bleachers sparkly dress girlnextdooritis Karlie. Kloss Kanye West Famous Loft 89 Emma Stone available at. Target Lily Aldridge Tribeca gym Ed Sheeran Jake Gyllenhaal Tree. Paine situation Christmas music darling I'm a nightmare. Dressed like a daydream 22 for me I think Met Gala. Romeo so basically Katy Perry mean refrigerator light. 
+          </p>
       </div>
     </Row>
   </Col>
