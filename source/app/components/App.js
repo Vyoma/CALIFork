8bf32fa7a54af9407db79a646748e038d0b75e29
Austin Riedel <riedel@Austins-MacBook-Pro-3.local>
2017-09-07 09:00:03 -0700

    Modify routes to implement a route for newly published assets

--- source/app/components/App.js
+++ source/app/components/App.js
@@ -1,5 +1,6 @@
 // REACT
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { Switch, Link, Route } from 'react-router-dom'
 import { ConnectedRouter } from 'react-router-redux'
 import { Helmet } from 'react-helmet'
@@ -7,7 +8,7 @@ import { Helmet } from 'react-helmet'
 // APP COMPONENTS 
 import Nav from '../containers/Nav'
 import Home from '../containers/Home/HomePage'
-import Asset from '../containers/Asset/AssetPage'
+import Asset from '../containers/Asset'
 import Publish from '../containers/Publish/PublishPage'
 import PublishedAsset from '../containers/PublishedAsset'
 import About from '../containers/About/AboutPage'
@@ -15,27 +16,29 @@ import About from '../containers/About/AboutPage'
 class App extends Component {
   render() {
     return (
-        <div>
-          <Helmet
-            htmlAttributes={{lang: "en", amp: undefined}} // amp takes no value
-            titleTemplate="%s | React App"
-            titleAttributes={{itemprop: "name", lang: "en"}}
-            meta={[
-              {name: "description", content: "Server side rendering example"},
-              {name: "viewport", content: "width=device-width, initial-scale=1"},
-            ]}
-          />
-          <Nav/>
-          <Switch>
-  	        <Route exact path='/' component={Home}/>
-            <Route path='/asset/:assetID' component={Asset}/>
-            <Route path='/publish' component={Publish}/>
-            <Route path='/publishedasset' component={PublishedAsset}/>
-            <Route path='/about' component={About}/>
-          </Switch>
-        </div>
+      <div>
+        <Helmet
+          htmlAttributes={{lang: "en", amp: undefined}} // amp takes no value
+          titleTemplate="%s | React App"
+          titleAttributes={{itemprop: "name", lang: "en"}}
+          meta={[
+            {name: "description", content: "Server side rendering example"},
+            {name: "viewport", content: "width=device-width, initial-scale=1"},
+          ]}
+        />
+        <Switch>
+          <Route path='/' component={Nav} />
+        </Switch>
+        <Switch>
+	        <Route exact path='/' component={Home}/>
+          <Route path='/asset/:assetID' component={Asset}/>
+          <Route exact path='/publish' component={Publish}/>
+          <Route path='/publish/asset/:assetID' component={PublishedAsset}/>
+          <Route path='/about' component={About}/>
+        </Switch>
+      </div>
     );
   }
 }
 
-export default App;
+export default App
--- source/client/routes.js
+++ source/client/routes.js
@@ -1,6 +1,6 @@
 import App from '../app/components/App'
 import Home from '../app/containers/Home/HomePage'
-import Asset from '../app/containers/Asset/AssetPage'
+import Asset from '../app/containers/Asset'
 import Publish from '../app/containers/Publish/PublishPage'
 import PublishedAsset from '../app/containers/PublishedAsset'
 import About from '../app/containers/About/AboutPage'
@@ -16,9 +16,10 @@ const routes = [
         component: Asset
       },
       { path: '/publish',
+        exact: true, 
         component: Publish
       },
-      { path: '/publishedasset',
+      { path: '/publish/asset/:assetID',
         component: PublishedAsset
       },
       { path: '/about',
