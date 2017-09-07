74b47e7c0a78bb5a5349e2c85be245ae1fc77689
Austin Riedel <riedel@Austins-MacBook-Pro-3.local>
2017-09-07 09:04:19 -0700

    Implement handler function for resetting search results on user clicking the Home nav button

--- source/app/containers/Nav/index.js
+++ source/app/containers/Nav/index.js
@@ -11,19 +11,50 @@ import Col from '../../components/Column' // CUSTOM COLUMN -> OPTIMIZED FOR SERV
 // STYLES
 import styles from '../../styles/styles.scss'
 
-export default () => (
-	<Container fluid={true} className={styles["tl-nav-container"]}>
-		<Row style={{ height: '100%', justifyContent: 'center', margin: 0}}>
-			<Col sm={7} md={7} className={styles["tl-nav-brand"]}>
-				<h2 className={styles["tl-nav-header"]}>
-					Cognitive Asset Library
-				</h2>
-			</Col>
-			<Col sm={5} md={5} className={styles["tl-nav-right"]}>
-			  <Link to={`/`} className={styles["tl-nav-button-border"]} kind='secondary'>Home</Link>
-			  <Link to={`/about`} className={styles["tl-nav-button-border"]} kind='secondary'>About Us</Link>
-			  <Link to={`/publish`} className={styles["tl-nav-button"]} kind='secondary'>Publish</Link>
-			</Col>
-		</Row>
-	</Container>
-)
+// ACTION CREATORS 
+import { resetHomeThunk } from '../../state/modules/assets' 
+
+class Nav extends Component {
+
+	// ENSURE THAT SEARCH RESULTS ARE RESET BY NAV TO HOME
+	handleHomeNavigation = () => {
+		const { pathname, resetHomeThunk } = this.props; 
+		if (pathname !== '/') {
+			console.log("NAVIGATING HOME, ACTION WILL FIRE ANYWAYS")
+			return; 
+		}
+
+		resetHomeThunk(pathname); 
+	}
+
+	render() {
+		return (
+			<Container fluid={true} className={styles["tl-nav-container"]}>
+				<Row style={{ height: '100%', justifyContent: 'center', margin: 0}}>
+					<Col sm={7} md={7} className={styles["tl-nav-brand"]}>
+						<h2 className={styles["tl-nav-header"]}>
+							Cognitive Asset Library
+						</h2>
+					</Col>
+					<Col sm={5} md={5} className={styles["tl-nav-right"]}>
+					  <Link to={`/`} onClick={this.handleHomeNavigation} className={styles["tl-nav-button-border"]} kind='secondary'>Home</Link>
+					  <Link to={`/about`} className={styles["tl-nav-button-border"]} kind='secondary'>About Us</Link>
+					  <Link to={`/publish`} className={styles["tl-nav-button"]} kind='secondary'>Publish</Link>
+					</Col>
+				</Row>
+			</Container>
+		)
+	}
+}
+
+const mapStateToProps = (state, { location }) => ({
+	pathname: location.pathname, 
+}) 
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  resetHomeThunk: () => {
+    dispatch(resetHomeThunk())
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Nav)
